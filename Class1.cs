940775e0af35ab7276a3421c28cacb6aa25fef00
abhinavmartha <abhinavmartha35@gmail.com>
2021-07-26 23:21:25 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Layer
+{
+    public class Customer
+    {
+            public string CustomerName { get; set; }
+            public int ContactNumber { get; set; }
+            public string Email { get; set; }
+            public DateTime DOB { get; set; }
+            public string PAN{ get; set; }
+            public int Aadhar { get; set; }
+ 
+    }
+}
+
